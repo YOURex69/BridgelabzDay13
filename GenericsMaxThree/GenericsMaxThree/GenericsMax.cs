5b391db17808f287d23712561699d939076d389d
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-17 18:03:35 +0530

    Refactor to add Generics Class

---
+++ GenericsMaxThree/GenericsMaxThree/GenericsMax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsMaxThree
+{
+    public class GenericsMax<T> where T:IComparable
+    {
+        public void ReturnMax(T v1, T v2, T v3)
+        {
+            if (v1.CompareTo(v2) > 0 && v1.CompareTo(v3) > 0 || v1.CompareTo(v2) >= 0 && v1.CompareTo(v3) > 0 || v1.CompareTo(v2) > 0 && v1.CompareTo(v3) >= 0)
+            {
+                Console.WriteLine(v1 + " is greter than " + v2 + " " + v3);
+            }
+            else if (v2.CompareTo(v1) > 0 && v2.CompareTo(v3) > 0 || v2.CompareTo(v1) >= 0 && v2.CompareTo(v3) > 0 || v2.CompareTo(v1) > 0 && v2.CompareTo(v3) >= 0)
+            {
+                Console.WriteLine(v2 + " is greter than " + v1 + " " + v3);
+            }
+            else if (v3.CompareTo(v1) > 0 && v3.CompareTo(v2) > 0 || v3.CompareTo(v2) >= 0 && v3.CompareTo(v1) > 0 || v3.CompareTo(v2) > 0 && v3.CompareTo(v2) >= 0)
+            {
+                Console.WriteLine(v3 + " is greter than " + v2 + " " + v1);
+            }
+        }
+    }
+}
---
+++ GenericsMaxThree/GenericsMaxThree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericsMaxThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericsMax<int> obj1 = new GenericsMax<int>();
+            obj1.ReturnMax(22, 233, 99);
+            GenericsMax<double> obj2 = new GenericsMax<double>();
+            obj2.ReturnMax(23.4, 565, 99.5);
+            GenericsMax<string> obj3 = new GenericsMax<string>();
+            obj3.ReturnMax("Seven", "Eleven", "Supreme");
+        }
+    }
+}
