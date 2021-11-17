b6f82ca8360f9daeeae1c68644801608cd4482ab
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-17 18:32:08 +0530

    Refactor uc3

---
+++ GenericMethodMaxOfThree/GenericMethodMaxOfThree/GetMaximum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericMethodMaxOfThree
+{
+    public class GetMaximum<T> where T:IComparable
+    {
+       public T v1, v2, v3;
+      public GetMaximum(T v1,T v2,T v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }
+
+
+
+        public void ReturnMax()
+        {
+            if ((v1.CompareTo(v2) > 0 && v1.CompareTo(v3) > 0) || v1.CompareTo(v2) >= 0 && v1.CompareTo(v3) > 0 || v1.CompareTo(v2) > 0 && v1.CompareTo(v3) >= 0)
+            {
+                Console.WriteLine(v1 + " is greter than " + v2 + " " + v3);
+            }
+            else if ((v2.CompareTo(v1) > 0 && v2.CompareTo(v3) > 0) || v2.CompareTo(v1) >= 0 && v2.CompareTo(v3) > 0 || v2.CompareTo(v1) > 0 && v2.CompareTo(v3) >= 0)
+            {
+                Console.WriteLine(v2 + " is greter than " + v1 + " " + v3);
+            }
+            else if ((v3.CompareTo(v1) > 0 && v3.CompareTo(v2) > 0) || v3.CompareTo(v2) >= 0 && v3.CompareTo(v1) > 0 || v3.CompareTo(v2) > 0 && v3.CompareTo(v2) >= 0)
+            {
+                Console.WriteLine(v3 + " is greter than " + v2 + " " + v1);
+            }
+        }
+
+    }
+}
---
+++ GenericMethodMaxOfThree/GenericMethodMaxOfThree/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+namespace GenericMethodMaxOfThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetMaximum<int> obj1 = new GetMaximum<int>(11,34,5);
+            obj1.ReturnMax();
+            GetMaximum<double> obj2 = new GetMaximum<double>(11.4,5.7,78.4);
+            obj2.ReturnMax();
+
+            GetMaximum<string> obj3 = new GetMaximum<string>("Monkey","money","Banana");
+            obj3.ReturnMax();
+
+        }
+    }
+}
