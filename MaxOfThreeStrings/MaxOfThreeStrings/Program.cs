e835b03b46576d54f29da5e09a8d2002c5522f1d
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-17 17:58:35 +0530

    Add uc3

---
+++ MaxOfThreeStrings/MaxOfThreeStrings/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MaxOfThreeStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxStrings obj1 = new MaxStrings();
+            obj1.ReturnMax("Animal", "Mongoose", "Anaconda");
+        }
+    }
+}
